586cb5f1ea133d017b40ada241f08d4984d8bc9f
isradev <u23271982@utp.edu.pe>
2025-05-05 16:14:58 -0500

    Se creo pool de conexiones

---
+++ src/main/java/org/contenido/persistencia/ConexionPool.java
@@ -0,0 +1,36 @@
+package org.contenido.persistencia;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConexionPool {
+    private static HikariDataSource codigoFuente;
+
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/BDPatrimonioU");
+        config.setUsername("usuario");
+        config.setPassword("contraseña");
+
+        config.setMaximumPoolSize(10); // conexiones máximas
+        config.setMinimumIdle(2);      // conexiones mínimas en reposo
+        config.setIdleTimeout(30000);  // 30 segundos
+        config.setMaxLifetime(1800000); // 30 minutos
+        config.setConnectionTimeout(30000); // timeout de espera
+
+        codigoFuente = new HikariDataSource(config);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return codigoFuente.getConnection();
+    }
+
+    public static void cerrarPool() {
+        if (codigoFuente != null) {
+            codigoFuente.close();
+        }
+    }
+}
