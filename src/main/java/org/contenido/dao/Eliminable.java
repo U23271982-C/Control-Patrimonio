7c3291fe49057048d7065027ff7a8ec853327c38
isradev <u23271982@utp.edu.pe>
2025-05-05 23:56:09 -0500

    Se segrego la interfaz CRUD

---
+++ src/main/java/org/contenido/dao/Eliminable.java
@@ -0,0 +1,5 @@
+package org.contenido.dao;
+
+public interface Eliminable <T> {
+    void eliminar (int idEntidad);
+}
---
+++ src/main/java/org/contenido/dao/Registrable.java
@@ -0,0 +1,5 @@
+package org.contenido.dao;
+
+public interface Registrable<T> {
+    void registrar(T entidad);
+}
