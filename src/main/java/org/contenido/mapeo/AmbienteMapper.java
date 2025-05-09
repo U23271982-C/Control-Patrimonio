717aebd19106dc428a87a467def5ed10a23129e9
isradev <u23271982@utp.edu.pe>
2025-05-08 22:54:36 -0500

    Se implemento correctamente mapperAmbiente

--- src/main/java/org/contenido/mapeo/AmbienteMapper.java
+++ src/main/java/org/contenido/mapeo/AmbienteMapper.java
@@ -9,11 +9,20 @@ public class AmbienteMapper implements Convertidor<Ambiente, AmbienteDTO>{
 
     @Override
     public AmbienteDTO convertirDTO(Ambiente modelo) {
-        return null;
+        AmbienteDTO dto = new AmbienteDTO();
+        dto.setId(modelo.getId());
+        dto.setNombre(modelo.getNombre());
+        if (modelo.getInmueble() != null) dto.setId_inmueble(modelo.getInmueble().getId());
+
+        return dto;
     }
 
     @Override
     public Ambiente convertirModelo(AmbienteDTO dto) {
+        Ambiente model = new Ambiente();
+        model.setId(dto.getId());
+        model.setNombre(dto.getNombre());
+        model.setInmueble(null);
         return null;
     }
 }
