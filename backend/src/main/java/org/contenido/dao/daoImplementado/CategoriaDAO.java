79572ec0352de09af8b8ff69de41c6ff7ba9037f
isradev <u23271982@utp.edu.pe>
2025-06-15 16:21:31 -0500

    registrar categoria

--- backend/src/main/java/org/contenido/dao/daoImplementado/CategoriaDAO.java
+++ backend/src/main/java/org/contenido/dao/daoImplementado/CategoriaDAO.java
@@ -1,14 +1,31 @@
 package org.contenido.dao.daoImplementado;
 
 import org.contenido.dao.DAO;
+import org.contenido.excepcion.PersistenciaExcepcion;
 import org.contenido.modelo.Categoria;
+import org.contenido.modelo.Responsable;
+import org.contenido.persistencia.ConexionPool;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public class CategoriaDAO implements DAO<Categoria> {
     @Override
     public void registrar(Categoria entidad) {
+        String sql = "{ CALL pa_Registrar_Categoria(?, ?) }";
+        try (Connection conn = ConexionPool.getConnection();
+             CallableStatement stmt = conn.prepareCall(sql)){
 
+            stmt.setString(1, entidad.getNombre());
+            stmt.setString(2, entidad.getDescripcion());
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new PersistenciaExcepcion(String.format("Error al registrar %s: ", Responsable.class.getName()), e);
+        }
     }
 
     @Override
