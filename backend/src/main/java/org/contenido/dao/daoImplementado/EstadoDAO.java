beb4bf29abe2932831a4d01c0f05a73a6477f7e4
isradev <u23271982@utp.edu.pe>
2025-06-15 16:17:18 -0500

    Se registro estado

--- backend/src/main/java/org/contenido/dao/daoImplementado/EstadoDAO.java
+++ backend/src/main/java/org/contenido/dao/daoImplementado/EstadoDAO.java
@@ -1,14 +1,30 @@
 package org.contenido.dao.daoImplementado;
 
 import org.contenido.dao.DAO;
+import org.contenido.excepcion.PersistenciaExcepcion;
 import org.contenido.modelo.Estado;
+import org.contenido.modelo.Responsable;
+import org.contenido.persistencia.ConexionPool;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public class EstadoDAO implements DAO<Estado> {
     @Override
     public void registrar(Estado entidad) {
+        String sql = "{ CALL pa_Registrar_Estado(?) }";
+        try (Connection conn = ConexionPool.getConnection();
+             CallableStatement stmt = conn.prepareCall(sql)){
 
+            stmt.setString(1, entidad.getTipo());
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new PersistenciaExcepcion(String.format("Error al registrar %s: ", Estado.class.getName()), e);
+        }
     }
 
     @Override
