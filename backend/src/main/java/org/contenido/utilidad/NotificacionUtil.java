bee8a76f470d2daac0af2d366b5f496296287bce
isradev <u23271982@utp.edu.pe>
2025-06-19 15:37:40 -0500

    creando excepcion superadmin

--- backend/src/main/java/org/contenido/utilidad/NotificacionUtil.java
+++ backend/src/main/java/org/contenido/utilidad/NotificacionUtil.java
@@ -8,20 +8,28 @@ import javax.swing.*;
 import java.util.Map;
 
 public class NotificacionUtil {
-    private record TipoError(String titulo, int tipo) {}
+    private record TipoError(String titulo, int tipo) {} // Record para encapsular el título y tipo de mensaje de error
 
     private static final Map<Class<? extends Exception>, TipoError> MAPEO_ERRORES = Map.of(
+            // Mapeo de excepciones a tipos de error para mostrar mensajes específicos
+            // Por cada tipo de excepción creada en /excepcion, se debe agregar aquí
             ValidacionExcepcion.class, new TipoError("Error de validación", JOptionPane.WARNING_MESSAGE),
             NegocioExcepcion.class, new TipoError("Error de negocio", JOptionPane.WARNING_MESSAGE),
             PersistenciaExcepcion.class, new TipoError("Error en la base de datos", JOptionPane.ERROR_MESSAGE)
     );
 
     public static void mostrarMensajeAfirmacion(String mensaje) {
+        // Este método se utiliza para notificar al usuario de una acción exitosa
+        // como la creación, actualización o eliminación de un registro.
         JOptionPane.showMessageDialog(null, mensaje, "Información", JOptionPane.INFORMATION_MESSAGE);
     }
 
     public static void mostrarError(Exception e) {
-        var tipoError = MAPEO_ERRORES.getOrDefault(e.getClass(),
+        // Este método se utiliza para notificar al usuario de un error ocurrido
+        // durante la ejecución de una acción, como la validación de datos o
+        // la interacción con la base de datos.
+        var tipoError = MAPEO_ERRORES.getOrDefault(
+                e.getClass(),
                 new TipoError("Error", JOptionPane.ERROR_MESSAGE));
 
         JOptionPane.showMessageDialog(null, e.getMessage(), tipoError.titulo(), tipoError.tipo());
